cae58e533406095f0f9dd3296aa27aceabcef0f8
OksanaHoloborodko <holoborodko.oksana@gmail.com>
2023-08-11 12:38:25 +0300

    feat: add html, js files

---
+++ js/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+let num1 = 100;
+let num2 = 5;
+
+let sum = num1 + num2;
+console.log(`sum: ${sum}`);
+
+let subtraction = num1 - num2;
+console.log(`subtraction: ${subtraction}`);
+
+let multiplication = num1 * num2;
+console.log(`multiplication: ${multiplication}`);
+
+let division = num1 / num2;
+console.log(`division: ${division}`);
+
+let exponentiation = num1 ** num2;
+console.log(`exponentiation: ${exponentiation}`);
