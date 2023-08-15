753559a9d848547d0627d30ec2cd3d31af1768ac
OksanaHoloborodko <holoborodko.oksana@gmail.com>
2023-08-15 19:21:53 +0300

    feat: add: Password checker, UAH to USD converter, Calculating cost of petrol

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
 'use strict';
 
+//Homework 2
+
+//Password checker
+let password = prompt('Enter your password');
+
+console.log(password.length > 7 && !isNaN(password[0]));
+
+//UAH to USD converter
+let rate = 0.027;
+let valueUah = prompt('Enter value in UAH');
+let valueUsd = +(valueUah * rate).toFixed(2);
+
+console.log(valueUsd);
+
+//Calculating cost of petrol
+let numOfLiters = +prompt('How many liters of petrol do you want to buy?');
+let costPerLiter = +prompt('Enter the cost per liter of petrol');
+let calculatedCost = (numOfLiters * costPerLiter).toFixed(2);
+
+console.log(`You have to pay ${calculatedCost}`);
+
+
+/*Homework 1
 let a = 100;
 let b = 5;
 
@@ -16,4 +39,4 @@ let division = a / b;
 console.log(`division: ${division}`);
 
 let exponentiation = a ** b;
-console.log(`exponentiation: ${exponentiation}`);
+console.log(`exponentiation: ${exponentiation}`);*/
