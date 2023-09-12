17089cf295c2ba47bf9455cd877bebf97f7d9f57
OksanaHoloborodko <holoborodko.oksana@gmail.com>
2023-09-12 23:10:23 +0300

    feat: add multiply function

--- HW 7/script.js
+++ HW 7/script.js
@@ -4,3 +4,10 @@
 function addThemAll(...args) {
     return args.reduce((acc, currentNum) => acc + currentNum, 0);
 }
+
+//Task 2
+function multiply(a) {
+    return function(b) {
+        return a * b;
+    }
+}
