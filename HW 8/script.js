c751f9ca9f9ed77b0c2f06d06fa5c7e6da7906f1
OksanaHoloborodko <holoborodko.oksana@gmail.com>
2023-09-16 20:38:46 +0300

    feat: binded context to object phone

--- HW 8/script.js
+++ HW 8/script.js
@@ -43,6 +43,13 @@ let phone = {
         console.log(`Introducing the ${this.brand} ${this.model} in a stunning ${this.color} hue. This sleek and powerful device boasts an impressive ${this.memory} of memory, ensuring you have ample space for all your apps, photos, and videos. `);
     },
     forecast() {
-        console.log(`The new model will cost ${this.price * 1.2} and will be released in ${this.year + 1}.`);
+        console.log(`The new model will cost ${this.price * 1.2} dollars and will be released in ${this.year + 1}.`);
     }
 }
+
+//Task 4
+let securedDescription = phone.description.bind(phone);
+let securedForecast = phone.forecast.bind(phone);
+
+setTimeout(securedDescription, 1000);
+setTimeout(securedForecast, 2000);
