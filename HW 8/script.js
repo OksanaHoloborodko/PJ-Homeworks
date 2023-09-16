ae2422e985b36b0c8ea6194ecda4c50f1dc18f76
OksanaHoloborodko <holoborodko.oksana@gmail.com>
2023-09-16 20:01:04 +0300

    feat: add detonatorTimer function (setTimeout)

--- HW 8/script.js
+++ HW 8/script.js
@@ -14,3 +14,19 @@ function detonatorTimer(delay) {
         }
     }, 1000);
 }
+
+//Task 2
+function detonatorTimer(delay) {
+    let count = delay;
+
+    const timerId = setTimeout(function tick() {
+        if (count > 0) {
+            console.log(count);
+            count--;
+            setTimeout(tick, 1000);
+        } else {
+            clearTimeout(timerId);
+            console.log('BOOM!');
+        }
+    }, 1000);
+}
