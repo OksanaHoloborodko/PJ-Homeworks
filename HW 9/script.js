630a4817f40e4bb917834d780fdd497493dca763
OksanaHoloborodko <106115158+OksanaHoloborodko@users.noreply.github.com>
2023-09-19 22:37:09 +0300

    fix

--- HW 9/script.js
+++ HW 9/script.js
@@ -3,7 +3,7 @@
 const userNames = ['Петро', 'Емма', 'Петро', 'Емма', 'Марта', 'Яна', 'Василь', 'Антон', 'Олена', 'Емма'];
 
 function filterUnique(array) {
-    return [... new Set(array)];
+    return [...new Set(array)];
 }
 
 console.log(filterUnique(userNames));
