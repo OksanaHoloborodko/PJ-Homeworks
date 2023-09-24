3a1f975876dafb5b86b4e29e3acef195cfb03264
OksanaHoloborodko <holoborodko.oksana@gmail.com>
2023-09-24 13:46:41 +0300

    feat: add function sorted by property and direction

--- HW 10/script.js
+++ HW 10/script.js
@@ -1,4 +1,47 @@
 'use strict';
+//Task 1
+const movies = [
+	{
+		movieName: 'The Thing',
+		releaseYear: 1982,
+		directedBy: 'Carpenter',
+		runningTimeInMinutes: 109,
+	},
+	{
+		movieName: 'Aliens',
+		releaseYear: 1986,
+		directedBy: 'Cameron',
+		runningTimeInMinutes: 137,
+	},
+	{
+		movieName: 'Men in Black',
+		releaseYear: 1997,
+		directedBy: 'Sonnenfeld',
+		runningTimeInMinutes: 98,
+	},
+	{
+		movieName: 'Predator',
+		releaseYear: 1987,
+		directedBy: 'McTiernan',
+		runningTimeInMinutes: 107,
+	},
+];
+
+function byProperty(property, direction) {
+	if(direction === '>') {
+        return (a, b) => {
+            return a[property] - b[property];
+        };
+    } else if(direction === '<') {
+        return (a, b) => {
+            return b[property] - a[property];
+        };
+    }
+}
+
+console.log(movies.sort(byProperty('releaseYear', '>')));
+console.log(movies.sort(byProperty('runningTimeInMinutes', '<')));
+console.log(movies.sort(byProperty('movieName', '>')));
 
 //Task 2
 function someFunction(str, n) {
